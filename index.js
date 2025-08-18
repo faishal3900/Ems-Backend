023e44bc25609df9657110c542da69f3ef5173f2
faishal3900 <faishal3900@gmail.com>
2025-08-18 12:52:55 +0530

    problem solve

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(
     origin: [
       "https://ems-frontend-cyan.vercel.app/",
       "http://localhost:5173",
-      "http://localhost:5174"
+      // "http://localhost:5174"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
